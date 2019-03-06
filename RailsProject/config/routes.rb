74c039f55b35616226adfad3b428324e5ed02dfb
Michelle <michelle.aldn@outlook.com>
2019-03-06 02:33:20 -0600

    Added a menu that is present on every page.

--- RailsProject/config/routes.rb
+++ RailsProject/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :battlegrounds, only: %i[index show]
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
+
+  root to: 'battlegrounds#index'
 end
