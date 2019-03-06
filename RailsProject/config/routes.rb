1498d8be71f4fc1f3d00746b4a4c525e20d12f21
Michelle <michelle.aldn@outlook.com>
2019-03-06 02:42:39 -0600

    Added a search controller.

--- RailsProject/config/routes.rb
+++ RailsProject/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'search/results'
   resources :battlegrounds, only: %i[index show]
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
