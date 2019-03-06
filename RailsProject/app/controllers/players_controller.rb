4778b04e5db7481d69f0290c7d0a781e75396846
Michelle <michelle.aldn@outlook.com>
2019-03-06 02:06:25 -0600

    Implemented the players controller show action.

--- RailsProject/app/controllers/players_controller.rb
+++ RailsProject/app/controllers/players_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class PlayersController < ApplicationController
   def index
+    @players = Player.includes(:battleground)
   end
 
   def show
+    @player = Player.includes(:battleground, :team).find(params[:id])
   end
 end
