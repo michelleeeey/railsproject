6fdc1a37befd994c4c25040104bf03dd1263dba2
Michelle <michelle.aldn@outlook.com>
2019-03-06 02:51:20 -0600

    Added a search form that is present on all pages.

--- RailsProject/config/routes.rb
+++ RailsProject/config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'search/results'
   resources :battlegrounds, only: %i[index show]
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
+
+  get 'search_results', to: 'search#results', as: 'search_results'
 
   root to: 'battlegrounds#index'
 end
