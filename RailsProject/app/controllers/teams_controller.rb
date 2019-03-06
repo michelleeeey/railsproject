55f4ba6f530ba74492b2cf4bba5ee452476c1334
Michelle <michelle.aldn@outlook.com>
2019-03-06 02:03:36 -0600

    Implemented the teams controller show action.

--- RailsProject/app/controllers/teams_controller.rb
+++ RailsProject/app/controllers/teams_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class TeamsController < ApplicationController
   def index
+    @teams = Team.includes(:battleground)
   end
 
   def show
+    @team = Team.includes(:battleground).find(params[:id])
   end
 end
