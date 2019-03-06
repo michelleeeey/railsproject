e950dbac89924d78adefaba11f2fdc9e949ecf04
Michelle <michelle.aldn@outlook.com>
2019-03-06 03:11:37 -0600

    Implemented the search result action.

--- RailsProject/app/controllers/search_controller.rb
+++ RailsProject/app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   def results
+    @query = params[:query]
+    @battleground_found = Battleground.where('name LIKE ?', "%#{@query}%")
+    @team_found = Team.includes(:battleground).where('name LIKE ?', "%#{@query}%")
+    @player_found = Player.includes(:battleground, :team).where('name LIKE ?', "%#{@query}%")
   end
 end
