29aab64bbfe3e831fb8a636426f83a801898c658
Michelle <michelle.aldn@outlook.com>
2019-03-06 03:14:02 -0600

    Scaffolded pages.

--- RailsProject/config/routes.rb
+++ RailsProject/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :pages
   resources :battlegrounds, only: %i[index show]
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
