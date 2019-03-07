bf52cf218f27c80c57758604e608afbd44eef5e5
Michelle <michelle.aldn@outlook.com>
2019-03-07 17:58:52 -0600

    Modified search controller to have a search category and its results view .

--- RailsProject/app/controllers/search_controller.rb
+++ RailsProject/app/controllers/search_controller.rb
@@ -2,9 +2,26 @@
 
 class SearchController < ApplicationController
   def results
-    @query = params[:query]
-    @battleground_found = Battleground.where('name LIKE ?', "%#{@query}%")
-    @team_found = Team.includes(:battleground).where('name LIKE ?', "%#{@query}%")
-    @player_found = Player.includes(:battleground, :team).where('name LIKE ?', "%#{@query}%")
+    @query = params[:q]
+    @battleground = Battleground.where('name LIKE ?', "%#{@query}%")
+    @team = Team.includes(:battleground).where('name LIKE ?', "%#{@query}%")
+    @player = Player.includes(:battleground, :team).where('name LIKE ?', "%#{@query}%")
+
+    @search_option = params[:search_option]
+
+    if @query == ''
+      @search_query = []
+    else
+      case @search_option
+      when 'Search All'
+        @search_query = @battleground + @team + @player
+      when 'Battleground'
+        @search_query = @battleground
+      when 'Team'
+        @search_query = @team
+      when 'Player'
+        @search_query = @player
+      end
+    end
   end
-end
+  end
